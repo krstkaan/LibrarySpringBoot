cd41a1db07ee65a1fba92b0a8b892f8b0bbea6bf
krstkaan <k.kaanakbulak@gmail.com>
2024-12-26 20:35:07 +0300

    First Commit

---
+++ src/main/java/com/sufaka/libraryspringboot/controller/BookController.java
@@ -0,0 +1,54 @@
+package com.sufaka.libraryspringboot.controller;
+
+import com.sufaka.libraryspringboot.model.Book;
+import com.sufaka.libraryspringboot.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    // Tüm kitapları getir
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Yeni bir kitap ekle
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+
+    // Belirli bir kitabı getir
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    // Belirli bir kitabı güncelle
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book bookDetails) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setPublishedDate(bookDetails.getPublishedDate());
+            book.setIsbn(bookDetails.getIsbn());
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    // Belirli bir kitabı sil
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+    }
+}
